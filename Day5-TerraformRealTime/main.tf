b367c435e6b17e820954685b4554f42bf4b1513a
ParameshwariV27 <parameshwaribv27@gmail.com>
2026-07-04 19:46:41 +0530

    Corrected file 39

--- Day5-TerraformRealTime/main.tf
+++ Day5-TerraformRealTime/main.tf
@@ -36,7 +36,7 @@ gateway_id = aws_internet_gateway.igw.id
 }
 }
 
-resource " aws_route_table_association" "rta1" {
+resource "aws_route_table_association" "rta1" {
 subnet_id = aws_subnet.sub1.id
 route_table_id = aws_route_table.RT.id
 }

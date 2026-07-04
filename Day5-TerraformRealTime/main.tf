55434834bc710645f2146864d6fedad5cbfe5823
ParameshwariV27 <parameshwaribv27@gmail.com>
2026-07-04 19:59:01 +0530

    corrected line 53

--- Day5-TerraformRealTime/main.tf
+++ Day5-TerraformRealTime/main.tf
@@ -46,7 +46,7 @@ name = "web"
 vpc_id = aws_vpc.myvpc.id
 
 ingress {
-dscription= "HTTP from VPC"
+description= "HTTP from VPC"
 from_port = 80
 to_port = 80
 protocol = "tcp"
@@ -63,7 +63,7 @@ egress {
 from_port = 0
 to_port =0
 protocol = "-1"
-cidr_block = ["0.0.0.0/0"]
+cidr_blocks = ["0.0.0.0/0"]
 }
 
 tags = {

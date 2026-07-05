800c733177f0012d943c892b7f080b39b4b70340
ParameshwariV27 <parameshwaribv27@gmail.com>
2026-07-06 00:03:25 +0530

    added lookup code

--- Day6/main.tf
+++ Day6/main.tf
@@ -8,10 +8,17 @@ description = "The AMI ID for the instance"
 
 variable "instance_type" {
 description = "The type of instance to launch"
+type = map(string)
+
+default = {
+"dev" = "t3.micro"
+"stage"="t3.medium"
+"prod" = "t3.small"
+}
 }
 
 module "ec2_instance" {
 source = "./modules/ec2_instance"
 ami = var.ami
-instance_type = var.instance_type
+instance_type = lookup(var.instance_type,terraform.workspace,"t3.small")
 }
